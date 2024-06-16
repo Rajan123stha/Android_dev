6cd88b7bddc68964db62f99f8054c6a281e8f889
Rajan <rajan1234stha@gmail.com>
2024-06-16 14:04:21 +0545

    relative layout

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -16,39 +16,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText username_editText, password_editText;
+    EditText emailInput, passwordInput;
 
-    Button blue_button;
-    Spinner random_spinner;
+    Button submitBtn;
+    Spinner genderSpinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        username_editText=findViewById(R.id.username_et);
-        password_editText=findViewById(R.id.pass_et);
-        blue_button=findViewById(R.id.btn_submit);
-        random_spinner.findViewById(R.id.spin);
+        emailInput=findViewById(R.id.email_input);
+        passwordInput=findViewById(R.id.password_input);
+        submitBtn=findViewById(R.id.submit_btn);
+        genderSpinner=findViewById(R.id.gender_spinner);
 
-        blue_button.setOnClickListener(new View.OnClickListener() {
+        submitBtn.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
-                String typedUsername= String.valueOf(username_editText.getText());
-                String typedPassword= String.valueOf(password_editText.getText());
-                Toast.makeText(MainActivity.this, "Username="+typedUsername, Toast.LENGTH_SHORT).show();
-
+                String email_input=String.valueOf(emailInput.getText());
+                String password_input=String.valueOf(passwordInput.getText());
+                Toast.makeText(MainActivity.this, "Email:"+email_input + "/n" + "Password:" + password_input, Toast.LENGTH_SHORT).show();
             }
         });
-        random_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, "Selected item:" + parent.getSelectedItem(), Toast.LENGTH_SHORT).show();
-            }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
 
     }
 }
